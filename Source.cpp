af1bb5858e38a11440bc25ab64a0b9e4b664fea5
CHANWANIT NUCHYOO <63010224@KMITL.AC.TH>
2020-08-13 20:24:58 +0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int x[2];
+void rectangleArea(int, int);
+int main()
+{
+		char Rec[2][10] = { "Width","Height" };
+			printf("\n---RECTANGLE AREA---\n");
+			int r;
+			for (r = 0; r < 2; r++)
+			{
+				printf("%s : ", Rec[r]);
+				scanf_s("%d", &x[r]);
+			}
+			rectangleArea(x[0], x[1]);
+			printf("\n---End---\n");
+}
+void rectangleArea(int width, int height)
+{
+	int i, j;
+	if (width <= 0 || height <= 0)
+	{
+		printf("BEEP! ERROR");
+	}
+	else
+	{
+		int area = width * height;
+		printf("Rectangle area : %.d\n",area);
+		for (i = 1; i <= height; i++)
+		{
+			for (j = 1; j <= width; j++)
+			{
+				if (i == 1 || i == height)
+				{
+					printf("* ");
+				}
+				else if (j == 1 || j == width)
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+			printf(" %d", i);
+			printf("\n");
+		}
+		printf("\n");
+		for (int k = 1; k <= width; k++)
+		{
+			printf("%d ",k);
+		}
+	}
+}
